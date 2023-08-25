9a038d9b180e41aa0c41286885d8b62f8587934c
მაცი <93665709+maci03@users.noreply.github.com>
2023-08-25 18:45:25 +0400

    end of project first part

--- src/App.js
+++ src/App.js
@@ -1,28 +1,24 @@
+import React, { useState, useEffect } from 'react';
 import './App.css';
 import MovieList from './Components/MovieList';
-
-import React, { useState, useEffect } from 'react';
-
-
-
 
 const App = () => {
   const [movies, setMovies] = useState([]);
 
-
-  useEffect(()=>{
+  useEffect(() => {
+    // Fetch movie data from an API (replace with a real API endpoint)
     fetch('https://api.example.com/movies')
-    .then(response => response.json())
-    .then(data => setMovies(data))
-    .catch(error => console.error('Error fetching movies:', error));
+      .then(response => response.json())
+      .then(data => setMovies(data))
+      .catch(error => console.error('Error fetching movies:', error));
   }, []);
 
-  return(
+  return (
     <div className="app">
       <h1>Movie Library</h1>
-      <MovieList movies = {movies} />
+      <MovieList movies={movies} />
     </div>
   );
 };
 
-export default App
+export default App;
--- src/Components/Movie.js
+++ src/Components/Movie.js
@@ -1,12 +1,12 @@
-import React from "react";
+import React from 'react';
 
-const Movie = ({title, year}) =>{
-    return(
-        <div className="Movie">
-            <h2>{title}</h2>
-            <p>Year: {year}</p>
-        </div>
-    );
+const Movie = ({ title, year }) => {
+  return (
+    <div className="movie">
+      <h2>{title}</h2>
+      <p>Year: {year}</p>
+    </div>
+  );
 };
 
 export default Movie;
