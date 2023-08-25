eeb6795f244b209ca784db9d358523d5335ccfa4
მაცი <93665709+maci03@users.noreply.github.com>
2023-08-25 18:29:46 +0400

    ended app page

--- src/App.js
+++ src/App.js
@@ -1,13 +1,28 @@
 import './App.css';
-import React from 'react';
-import MyComponent from './Components/home/Header';
+import MovieList from './Components/MovieList';
 
-function App() {
-  return (
-    <div className="App">
-      
+import React, { useState, useEffect } from 'react';
+
+
+
+
+const App = () => {
+  const [movies, setMovies] = useState([]);
+
+
+  useEffect(()=>{
+    fetch('https://api.example.com/movies')
+    .then(response => response.json())
+    .then(data => setMovies(data))
+    .catch(error => console.error('Error fetching movies:', error));
+  }, []);
+
+  return(
+    <div className="app">
+      <h1>Movie Library</h1>
+      <MovieList movies = {movies} />
     </div>
   );
-}
+};
 
-export default App;
+export default App
